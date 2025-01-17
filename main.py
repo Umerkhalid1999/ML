1c44875c6ed150b7adc817ce93e99890c5b02bb6
Umar Khalid Mirza <umertalks786@gmail.com>
2025-01-17 14:48:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ st.markdown(
     /* Button styling and hover transition */
     .stButton button {
         background-color: #4CAF50;
-        color: white;
+        color: black;
         border: none;
         padding: 10px 20px;
         text-align: center;
